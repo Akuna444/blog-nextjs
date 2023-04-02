73560a8f091a57d822c60052eafb3b2f3bc14a4f
Akuna444 <aklilsolomon3@gmail.com>
2023-04-02 22:38:26 +0300

    render: post items

--- components/posts/posts-grid.js
+++ components/posts/posts-grid.js
@@ -1,10 +1,15 @@
+import PostItem from "./post-item";
+import classes from "./posts-grid.module.css";
+
 function PostsGrid(props) {
   const { posts } = props;
   return (
-    <ul>
-      {posts.map((data) => (
-        <PostItem />
+    <ul className={classes.grid}>
+      {posts.map((post) => (
+        <PostItem key={post.slug} post={post} />
       ))}
     </ul>
   );
 }
+
+export default PostsGrid;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,47 @@
 import { Fragment } from "react";
 
 import Hero from "../components/home-page/hero";
+import FeaturedPosts from "../components/home-page/featured-posts";
+
+const DUMMY_DATA = [
+  {
+    title: "Getting Started With NextJS",
+    image: "getting-started-nextjs.png",
+    slug: "getting-started-with-nextjs",
+    date: "2023-04-01",
+    excerpt:
+      "NextJS is a React frame work that helps to build fullstack react with SSR",
+  },
+  {
+    title: "Getting Started With NextJS",
+    image: "getting-started-nextjs.png",
+    slug: "getting-started-with-nextjs2",
+    date: "2023-04-01",
+    excerpt:
+      "NextJS is a React frame work that helps to build fullstack react with SSR",
+  },
+  {
+    title: "Getting Started With NextJS",
+    image: "getting-started-nextjs.png",
+    slug: "getting-started-with-nextjs3",
+    date: "2023-04-01",
+    excerpt:
+      "NextJS is a React frame work that helps to build fullstack react with SSR",
+  },
+  {
+    title: "Getting Started With NextJS",
+    image: "getting-started-nextjs.png",
+    slug: "getting-started-with-nextjs4",
+    date: "2023-04-01",
+    excerpt:
+      "NextJS is a React frame work that helps to build fullstack react with SSR",
+  },
+];
 function HomePage() {
   return (
     <Fragment>
       <Hero />
+      <FeaturedPosts posts={DUMMY_DATA} />
     </Fragment>
   );
 }
