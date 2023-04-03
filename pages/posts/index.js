6b7ec3038c296a2fab2031cc0c2d83c8b1f5c2e4
Akuna444 <aklilsolomon3@gmail.com>
2023-04-03 10:28:23 +0300

    render: all posts

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,5 +1,41 @@
+import AllPosts from "../../components/posts/all-posts";
+
+const DUMMY_DATA = [
+  {
+    title: "Getting Started With NextJS",
+    image: "getting-started-nextjs.png",
+    slug: "getting-started-with-nextjs",
+    date: "2023-04-01",
+    excerpt:
+      "NextJS is a React frame work that helps to build fullstack react with SSR",
+  },
+  {
+    title: "Getting Started With NextJS",
+    image: "getting-started-nextjs.png",
+    slug: "getting-started-with-nextjs2",
+    date: "2023-04-01",
+    excerpt:
+      "NextJS is a React frame work that helps to build fullstack react with SSR",
+  },
+  {
+    title: "Getting Started With NextJS",
+    image: "getting-started-nextjs.png",
+    slug: "getting-started-with-nextjs3",
+    date: "2023-04-01",
+    excerpt:
+      "NextJS is a React frame work that helps to build fullstack react with SSR",
+  },
+  {
+    title: "Getting Started With NextJS",
+    image: "getting-started-nextjs.png",
+    slug: "getting-started-with-nextjs4",
+    date: "2023-04-01",
+    excerpt:
+      "NextJS is a React frame work that helps to build fullstack react with SSR",
+  },
+];
 function AllPostsPage() {
-  return;
+  return <AllPosts posts={DUMMY_DATA} />;
 }
 
 export default AllPostsPage;
