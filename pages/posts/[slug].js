7b58eb18119650d2705a37b48d68e217e74135ec
Akuna444 <aklilsolomon3@gmail.com>
2023-04-07 12:11:27 +0300

    replace: dummy data with md

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,7 +1,32 @@
 import PostContent from "../../components/posts/post-detail/post-content";
+import { getPostData, getPostFiles } from "../../lib/post-util";
 
-function PostDetailPage() {
-  return <PostContent />;
+function PostDetailPage(props) {
+  return <PostContent post={props.post} />;
 }
 
+export function getStaticProps(context) {
+  const { params } = context;
+  const { slug } = params;
+  const postData = getPostData(slug);
+  return {
+    props: {
+      post: postData,
+    },
+    revalidate: 600,
+  };
+}
+
+export function getStaticPaths() {
+  const postFileNames = getPostFiles();
+  const slug = postFileNames.map((fileNames) => fileNames.replace(/\.md$/, ""));
+  return {
+    paths: slug.map((slug) => {
+      params: {
+        slug: slug;
+      }
+    }),
+    fallback: false,
+  };
+}
 export default PostDetailPage;
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,41 +1,17 @@
 import AllPosts from "../../components/posts/all-posts";
+import { getAllPosts } from "../../lib/post-util";
 
-const DUMMY_DATA = [
-  {
-    title: "Getting Started With NextJS",
-    image: "getting-started-nextjs.png",
-    slug: "getting-started-with-nextjs",
-    date: "2023-04-01",
-    excerpt:
-      "NextJS is a React frame work that helps to build fullstack react with SSR",
-  },
-  {
-    title: "Getting Started With NextJS",
-    image: "getting-started-nextjs.png",
-    slug: "getting-started-with-nextjs2",
-    date: "2023-04-01",
-    excerpt:
-      "NextJS is a React frame work that helps to build fullstack react with SSR",
-  },
-  {
-    title: "Getting Started With NextJS",
-    image: "getting-started-nextjs.png",
-    slug: "getting-started-with-nextjs3",
-    date: "2023-04-01",
-    excerpt:
-      "NextJS is a React frame work that helps to build fullstack react with SSR",
-  },
-  {
-    title: "Getting Started With NextJS",
-    image: "getting-started-nextjs.png",
-    slug: "getting-started-with-nextjs4",
-    date: "2023-04-01",
-    excerpt:
-      "NextJS is a React frame work that helps to build fullstack react with SSR",
-  },
-];
-function AllPostsPage() {
-  return <AllPosts posts={DUMMY_DATA} />;
+function AllPostsPage(props) {
+  return <AllPosts posts={props.posts} />;
 }
 
+export function getStaticProps() {
+  const allPosts = getAllPosts();
+
+  return {
+    props: {
+      posts: allPosts,
+    },
+  };
+}
 export default AllPostsPage;
