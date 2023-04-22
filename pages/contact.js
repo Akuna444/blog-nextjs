50da81fc589ad0c7015140816ad5d7a63edbff3a
Akuna444 <aklilsolomon3@gmail.com>
2023-04-22 12:20:04 +0300

    add: metadata to all pages

--- pages/contact.js
+++ pages/contact.js
@@ -1,6 +1,16 @@
-import ContactForm from "../components/contact/contact";
+import { Fragment } from "react";
+import Head from "next/head";
+import ContactForm from "../components/contact/contact-form";
 function ContactPage() {
-  return <ContactForm />;
+  return (
+    <Fragment>
+      <Head>
+        <title>Contact Page</title>
+        <meta name="description" content="This your contact page to find me" />
+      </Head>
+      <ContactForm />
+    </Fragment>
+  );
 }
 
 export default ContactPage;
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,8 +1,21 @@
+import { Fragment } from "react";
+import Head from "next/dist/next-server/lib/head";
 import AllPosts from "../../components/posts/all-posts";
 import { getAllPosts } from "../../lib/post-util";
 
 function AllPostsPage(props) {
-  return <AllPosts posts={props.posts} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>All Posts</title>
+        <meta
+          name="description"
+          content="This is where you  can find all posts"
+        />
+      </Head>
+      <AllPosts posts={props.posts} />
+    </Fragment>
+  );
 }
 
 export function getStaticProps() {
